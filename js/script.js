9ee817fa8c0f16a58bfcba9991732ea42c80b26c
Daniel Totten <TottenDC@gmail.com>
2018-09-20 13:33:54 -0400

    Set up job title section
    Added "other" input to HTML
    Hide from display
    Created event to dispaly if selected

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@
 //Add autofocus to first input element as an attribute
 const nameInput = document.querySelector('#name');
 nameInput.setAttribute('autofocus', '');
+
+const otherJobInput = document.querySelector('#other-title');
+otherJobInput.style.display = 'none';
+
+const titleInput = document.querySelector('#title');
+titleInput.addEventListener('change', () => {
+  const userSelection = titleInput.value;
+  if (userSelection === 'other') {
+    otherJobInput.style.display = '';
+  } else {
+    otherJobInput.style.display = 'none';
+  }
+});
