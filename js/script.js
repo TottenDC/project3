552a7862bb2f99081635b4306c90c0cffd77cc24
Daniel Totten <TottenDC@gmail.com>
2018-09-20 16:33:23 -0400

    Set up T-shirt color system
    Hide label and options
    Create conditional function for displaying options
    Set up event handler to display color options based on theme choice

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 const nameInput = document.querySelector('#name');
 nameInput.setAttribute('autofocus', '');
 
+//Hide "other job role" input and display when selected
 const otherJobInput = document.querySelector('#other-title');
 otherJobInput.style.display = 'none';
 
@@ -14,5 +15,44 @@ titleInput.addEventListener('change', () => {
     otherJobInput.style.display = '';
   } else {
     otherJobInput.style.display = 'none';
+  }
+});
+
+//Hide color label and options
+const tshirtDesignInput = document.querySelector('#design');
+const tshirtColorInput = document.querySelector('#color');
+const tshirtColorLabel = tshirtColorInput.previousElementSibling;
+const tshirtColorOptions = tshirtColorInput.children;
+function tshirtDisplayOn() {
+  tshirtColorLabel.style.display = '';
+  tshirtColorInput.style.display = '';
+}
+function tshirtDisplayOff() {
+  tshirtColorLabel.style.display = 'none';
+  tshirtColorInput.style.display = 'none';
+}
+//The 'selected' attribute makes it responsive to the options available
+function tshirtOptionDisplay(indexStart, indexEnd) {
+  for (i=0; i<tshirtColorOptions.length; i++) {
+    tshirtColorOptions[i].style.display = 'none';
+    tshirtColorOptions[i].removeAttribute('selected');
+  }
+  tshirtColorOptions[indexStart].setAttribute('selected', '');
+  for (i=indexStart; i <= indexEnd; i++) {
+    tshirtColorOptions[i].style.display = '';
+  }
+}
+tshirtDisplayOff();
+//Create conditional selection of color based on design 
+tshirtDesignInput.addEventListener('change', () => {
+  const userSelection = tshirtDesignInput.value;
+  if (userSelection === 'js puns') {
+    tshirtDisplayOn();
+    tshirtOptionDisplay(0, 2);
+  } else if (userSelection === 'heart js') {
+    tshirtDisplayOn();
+    tshirtOptionDisplay(3, 5);
+  } else {
+    tshirtDisplayOff();
   }
 });
