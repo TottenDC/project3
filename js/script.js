7419ebb4a5d1f8d6c522b43283d25c7261b46649
Daniel Totten <TottenDC@gmail.com>
2018-09-20 10:45:19 -0400

    Add autofocus to first form element

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 /* FSJS Unit 3 Project - Interactive Form */
+
+//Add autofocus to first input element as an attribute
+const nameInput = document.querySelector('#name');
+nameInput.setAttribute('autofocus', '');
